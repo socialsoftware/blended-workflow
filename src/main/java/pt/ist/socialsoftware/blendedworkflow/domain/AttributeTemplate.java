96449283cf5a56571f49bc21449d9e33e861b907
DavidePassinhas <davide.passinhas@ist.utl.pt>
2012-01-23 15:28:36 +0000

    blendedworkflow.dml

---
+++ src/main/java/pt/ist/socialsoftware/blendedworkflow/domain/AttributeTemplate.java
@@ -0,0 +1,11 @@
+package pt.ist.socialsoftware.blendedworkflow.domain;
+
+public class AttributeTemplate extends AttributeTemplate_Base {
+	
+	public enum AttributeType {STRING, NUMBER, BOOLEAN};
+    
+    public  AttributeTemplate() {
+        super();
+    }
+    
+}
---
+++ src/main/java/pt/ist/socialsoftware/blendedworkflow/domain/RelationTemplate.java
@@ -0,0 +1,11 @@
+package pt.ist.socialsoftware.blendedworkflow.domain;
+
+public class RelationTemplate extends RelationTemplate_Base {
+	
+	public enum RelationCardinality { MANY, ZERO_OR_ONE, ONE}
+    
+    public  RelationTemplate() {
+        super();
+    }
+    
+}
