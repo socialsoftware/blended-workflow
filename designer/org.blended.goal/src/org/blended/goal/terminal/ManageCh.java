0c07e74777f0a1752b8e20a8ff26ad3c8d07b8ad
António Rito Silva <Rito.Silva@ist.utl.pt>
2015-07-17 15:14:52 +0100

    include libs and exclude generated

--- designer/org.blended.goal/src/org/blended/goal/terminal/ManageCh.java
+++ designer/org.blended.goal/src/org/blended/goal/terminal/ManageCh.java
@@ -1,12 +1,16 @@
 package org.blended.goal.terminal;
 
-import pt.ist.socialsoftware.blendedworkflow.domain.GoalModel;
+import org.blended.goal.goal.GoalModel;
 
 public class ManageCh {
-	public static void specification(GoalModel model, String name, CommandCh cmd) throws ValueException  {
-		if (cmd.value != null) {
-			model.getSpecification().setName(cmd.value);
-			ConsoleManagement.write(name, "New name for specification:" + cmd.value);
-		} else throw new ValueException("Parameter not valid: The following option is required: -v working with specifications");
-	}
+    public static void specification(GoalModel model, String name,
+            CommandCh cmd) throws ValueException {
+        if (cmd.value != null) {
+            model.getSpecification().setName(cmd.value);
+            ConsoleManagement.write(name,
+                    "New name for specification:" + cmd.value);
+        } else
+            throw new ValueException(
+                    "Parameter not valid: The following option is required: -v working with specifications");
+    }
 }
--- designer/org.blended.goal/src/org/blended/goal/terminal/ManageRm.java
+++ designer/org.blended.goal/src/org/blended/goal/terminal/ManageRm.java
@@ -1,21 +1,24 @@
 package org.blended.goal.terminal;
 
-import pt.ist.socialsoftware.blendedworkflow.domain.GoalModel;
+import org.blended.goal.goal.GoalModel;
 
 public class ManageRm {
-	public static void entity(GoalModel model, String name, CommandRm cmd) throws ValueException {
-//		if (cmd.value != null) {
-//			Entity ent = model.getEntities().stream()
-//				.filter(e -> e.getName().equals(cmd.value))
-//				.findFirst().get();
-//				//.forEach(e -> model.getEntities().remove(e));
-//			if (ent != null) {
-//				if (model.getEntities().remove(ent))
-//					ConsoleManagement.write(name, "Operation performed");
-//				else ConsoleManagement.write(name, "Operation NOT performed");	
-//			} 
-//			else ConsoleManagement.write(name, "Operation NOT performed. Entity not found");	
-//		} else throw new ValueException("Parameter not valid: The following option is required: -v working with specifications");
-	}
+    public static void entity(GoalModel model, String name, CommandRm cmd)
+            throws ValueException {
+        // if (cmd.value != null) {
+        // Entity ent = model.getEntities().stream()
+        // .filter(e -> e.getName().equals(cmd.value))
+        // .findFirst().get();
+        // //.forEach(e -> model.getEntities().remove(e));
+        // if (ent != null) {
+        // if (model.getEntities().remove(ent))
+        // ConsoleManagement.write(name, "Operation performed");
+        // else ConsoleManagement.write(name, "Operation NOT performed");
+        // }
+        // else ConsoleManagement.write(name, "Operation NOT performed. Entity
+        // not found");
+        // } else throw new ValueException("Parameter not valid: The following
+        // option is required: -v working with specifications");
+    }
 
 }
