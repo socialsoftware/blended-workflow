32970213402f1e2e8803201d60b73dec27bf95fc
Vicente García <vicegd@gmail.com>
2015-07-21 09:14:09 +0200

    ConsoleManagement in common

---
+++ designer/org.blended.common/src/org/blended/common/utils/ConsoleManagement.java
@@ -0,0 +1,77 @@
+package org.blended.common.utils;
+
+import java.io.IOException;
+
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.IOConsole;
+import org.eclipse.ui.console.IOConsoleOutputStream;
+
+public class ConsoleManagement {
+	public static String DATA_CONSOLE = "Data Console";
+	public static String CONDITION_CONSOLE = "Condition Console";
+	public static String ACTIVITY_CONSOLE = "Activity Console";
+	public static String GOAL_CONSOLE = "Goal Console";
+	
+	private static final RGB DEFAULT_COLOR = new RGB(0, 0, 0);
+	private static final RGB OUTPUT = new RGB(0, 0, 200);
+	private static final RGB OUTPUT_DATA = new RGB(0, 102, 51);
+	private static final RGB CARET = new RGB(100, 100, 100);
+	
+	public static IOConsole findConsole(String name) {
+		ConsolePlugin plugin = ConsolePlugin.getDefault();
+
+		IConsoleManager conMan = plugin.getConsoleManager();
+		IConsole[] existing = conMan.getConsoles();
+		for (int i = 0; i < existing.length; i++)
+			if (name.equals(existing[i].getName()))
+				return (IOConsole) existing[i];
+		ImageDescriptor img = null;
+		IOConsole myConsole = new IOConsole(name, img);
+		conMan.addConsoles(new IConsole[] { myConsole });	
+		return myConsole;
+	}
+	
+	public static void write(String name, Boolean rn, TypeOutput type, String message) {
+		IOConsole myConsole = findConsole(name);
+		IOConsoleOutputStream out = myConsole.newOutputStream();
+		out.setActivateOnWrite(true);
+		switch (type) {
+			case Output: out.setColor(createColor(OUTPUT)); break;
+			case OutputData: out.setColor(createColor(OUTPUT_DATA)); break;
+			case Caret: out.setColor(createColor(CARET)); break;
+			default: out.setColor(createColor(DEFAULT_COLOR)); break;
+		}
+		try {
+			if (rn) message += "\r\n";
+			if (type == TypeOutput.OutputData) message = "-> " + message;
+			out.write(message);
+		} catch (IOException e) {
+			//e.printStackTrace();
+		}	
+	}
+	
+	public static void write(String name, TypeOutput type, String message) {
+		write(name, true, type, message);
+	}
+	
+	public static void write(String name, String message) {
+		write(name, TypeOutput.Output, message);
+	}
+	
+	private static Color createColor(RGB rgb) {
+		if(rgb == null)
+			rgb = DEFAULT_COLOR;
+		return new Color(Display.getCurrent(), rgb);
+	}
+	
+	public static enum TypeOutput {
+		Default, Output, OutputData, Caret
+	}
+
+}
