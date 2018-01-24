bc9d7945214f8a93d1c5c9b7915bcf1978a2cda2
Guilherme Ramos <ramosguilhermefilipe@gmail.com>
2018-01-24 16:02:41 +0000

    Added API endpoint to create persons in Engine

--- engine/src/main/java/pt/ist/socialsoftware/blendedworkflow/controller/ResourceModelController.java
+++ engine/src/main/java/pt/ist/socialsoftware/blendedworkflow/controller/ResourceModelController.java
@@ -1,12 +1,8 @@
 package pt.ist.socialsoftware.blendedworkflow.controller;
 
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,22 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import pt.ist.socialsoftware.blendedworkflow.domain.Attribute;
-import pt.ist.socialsoftware.blendedworkflow.domain.Dependence;
-import pt.ist.socialsoftware.blendedworkflow.domain.Entity;
-import pt.ist.socialsoftware.blendedworkflow.domain.Product;
-import pt.ist.socialsoftware.blendedworkflow.domain.RelationBW;
-import pt.ist.socialsoftware.blendedworkflow.domain.Rule;
-import pt.ist.socialsoftware.blendedworkflow.service.design.DesignInterface;
-import pt.ist.socialsoftware.blendedworkflow.service.dto.AttributeDTO;
-import pt.ist.socialsoftware.blendedworkflow.service.dto.DependenceDTO;
-import pt.ist.socialsoftware.blendedworkflow.service.dto.EntityDTO;
-import pt.ist.socialsoftware.blendedworkflow.service.dto.ProductDTO;
-import pt.ist.socialsoftware.blendedworkflow.service.dto.RelationDTO;
-import pt.ist.socialsoftware.blendedworkflow.service.dto.RuleDTO;
+import pt.ist.socialsoftware.blendedworkflow.service.design.ResourceInterface;
+import pt.ist.socialsoftware.blendedworkflow.service.dto.PersonDTO;
 
 @RestController
 @RequestMapping(value = "/specs/{specId}/resourcemodel")
 public class ResourceModelController {
 	private static Logger log = LoggerFactory.getLogger(ResourceModelController.class);
+
+	@RequestMapping(value = "/persons", method = RequestMethod.POST)
+	public ResponseEntity<PersonDTO> createPerson(@PathVariable("specId") String specId,
+														@RequestBody PersonDTO personDTO) {
+		log.debug("createPerson entityExtId:{}, spec: {}, name:{}, bodytext:{}", personDTO.getExtId(), personDTO.getSpecId(), personDTO.getName(),
+				personDTO.getBody());
+
+		ResourceInterface adi = ResourceInterface.getInstance();
+
+		// FIXME: Person person = adi.createPerson(attDTO);
+
+		return new ResponseEntity<>(personDTO, HttpStatus.CREATED);
+	}
 }
