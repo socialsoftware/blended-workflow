44d42a8a977f476fcc585505ca0e9f11906cca61
Guilherme Ramos <ramosguilhermefilipe@gmail.com>
2018-02-21 22:26:20 +0000

    Added assignments RESPONSIBLE FOR and INFORMS to both Entities and Attributes in dml

---
+++ engine/resources/src/main/java/pt/ist/socialsoftware/blendedworkflow/resources/domain/RALExpression.java
@@ -0,0 +1,9 @@
+package pt.ist.socialsoftware.blendedworkflow.resources.domain;
+
+public class RALExpression extends RALExpression_Base {
+    
+    public RALExpression() {
+        super();
+    }
+    
+}
