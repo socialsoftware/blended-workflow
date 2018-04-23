10c4e6eb7a29a2ef192c3452f16e52ef280fb2de
Guilherme Ramos <ramosguilhermefilipe@gmail.com>
2018-04-23 18:39:59 +0100

    Updated the security adaptars to reflect the correct status codes

--- engine/bw-resources/src/main/java/pt/ist/socialsoftware/blendedworkflow/resources/config/BlendedWebSecurityConfigurerAdapter.java
+++ engine/bw-resources/src/main/java/pt/ist/socialsoftware/blendedworkflow/resources/config/BlendedWebSecurityConfigurerAdapter.java
@@ -17,16 +17,21 @@ import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import pt.ist.socialsoftware.blendedworkflow.resources.security.*;
 
-
+@Configuration
 @EnableWebSecurity
 public class BlendedWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
     private static Logger logger = LoggerFactory.getLogger(BlendedWebSecurityConfigurerAdapter.class);
 
     private UserDetailsService userDetailsService;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private RESTAuthenticationEntryPoint authenticationEntryPoint;
+    @Autowired
+    private RESTAuthenticationSuccessHandler authenticationSuccessHandler;
 
     public BlendedWebSecurityConfigurerAdapter(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userDetailsService = userDetailsService;
@@ -43,9 +48,13 @@ public class BlendedWebSecurityConfigurerAdapter extends WebSecurityConfigurerAd
                 .antMatchers("/").authenticated()
 //                .anyRequest().authenticated()
                 .and()
+            .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .and()
             .formLogin()
                 .loginPage("/login")
                 .permitAll()
+                .successHandler(authenticationSuccessHandler)
                 .and()
             .logout()
                 .permitAll()
