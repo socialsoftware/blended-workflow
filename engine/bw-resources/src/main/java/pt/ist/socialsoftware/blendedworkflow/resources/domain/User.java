d4c587ca5d94d0d9828eb3c7a782a6f6b1f87d3d
Guilherme Ramos <ramosguilhermefilipe@gmail.com>
2018-05-04 22:55:38 +0100

    Fixed the static method in User class to get the authenticated user

--- engine/bw-resources/src/main/java/pt/ist/socialsoftware/blendedworkflow/resources/domain/User.java
+++ engine/bw-resources/src/main/java/pt/ist/socialsoftware/blendedworkflow/resources/domain/User.java
@@ -14,6 +14,9 @@ import pt.ist.socialsoftware.blendedworkflow.resources.security.BlendedUserDetai
 import pt.ist.socialsoftware.blendedworkflow.resources.service.RMErrorType;
 import pt.ist.socialsoftware.blendedworkflow.resources.service.RMException;
 
+import java.security.Principal;
+import java.util.Optional;
+
 public class User extends User_Base {
     private static Logger logger = LoggerFactory.getLogger(User.class);
 
@@ -28,17 +31,18 @@ public class User extends User_Base {
         setPassword(bCryptPasswordEncoder.encode(password));
     }
 
-    public static User getAuthenticatedUser() {
+    public static Optional<User> getAuthenticatedUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication != null) {
-            BlendedUserDetails userDetails = null;
-            Object principal = authentication.getPrincipal();
-            if (principal instanceof BlendedUserDetails) {
-                userDetails = (BlendedUserDetails) principal;
-                return userDetails.getUser();
-            }
+            Optional<User> user = getUserByUsername(authentication.getName());
+
+            return user;
         }
-        return null;
+        return Optional.empty();
+    }
+
+    public static Optional<User> getUserByUsername(String username) {
+        return BlendedWorkflow.getInstance().getUsersSet().stream().filter(u -> u.getUsername().equals(username)).findFirst();
     }
 
     public void delete() {
