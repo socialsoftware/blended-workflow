5ee2049f8793206caccd958fd9f07239ac187bad
Antonio Rito Silva <Rito.Silva@ist.utl.pt>
2018-07-28 14:18:08 +0100

    React UI data view: dependencies

---
+++ frontend/bw-react/src/components/executor/dataview/DependenceInstance.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Tab } from '../../util/Tab'
+import { RepositoryService } from '../../../services/RepositoryService'
+import { EntityInstanceLink } from './EntityInstanceLink'
+
+export class DependenceInstance extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            entityInstances: []
+        };
+    }
+
+    componentDidMount() {
+        const service = new RepositoryService();
+        service.getEntityInstancesForDependence(this.props.entityInstance.externalId,this.props.dependence.extId).then(response => {
+            this.setState({ 
+                entityInstances: response.data 
+            });
+        });
+    }
+
+    render() {
+    const dependenceEntityInstances = this.state.entityInstances == null ? 'NONE' : this.state.entityInstances.map(ei => <EntityInstanceLink key={ei.id} entityInstance={ei} />);
+        return (
+            <div>
+                <Tab /><Tab />{this.props.dependence.path}: {dependenceEntityInstances}
+            </div>
+        )
+    }
+}
---
+++ frontend/bw-react/src/components/executor/dataview/EntityInstanceLink.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { OpenCloseButton } from './OpenCloseButton'
+import { EntityInstance } from './EntityInstance';
+
+export class EntityInstanceLink extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            open: false,
+        };
+
+        this.openCloseLink = this.openCloseLink.bind(this);
+    }
+
+    openCloseLink() {
+        this.setState({
+            open: !this.state.open
+        })
+    }
+
+    render() {
+        return (
+            <span>
+                {this.props.entityInstance.entity.name} [{this.props.entityInstance.id}] <OpenCloseButton open={this.state.open} onClick={this.openCloseLink} />
+                {this.state.open && <EntityInstance entityInstance={this.props.entityInstance} />}
+            </span>
+        )
+    }
+}
