bcd075ac1d0d5eff2828441a361941bb2e4a9881
BernardoFuret <bernardo.furet@tecnico.ulisboa.pt>
2018-12-18 00:00:53 +0000

    Fix generation of nested <a> tags. Some cleanup.

--- frontend/bw-react/src/components/Header.js
+++ frontend/bw-react/src/components/Header.js
@@ -1,36 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
+import { RepositoryService } from '../services/RepositoryService';
+
 import { setSpecifications } from '../actions/setSpecifications';
 import { selectSpecification } from '../actions/selectSpecification';
 import { setInstances } from '../actions/setInstances';
 import { selectInstance } from '../actions/selectInstance';
 import { setUsers } from '../actions/setUsers';
 import { selectUser } from '../actions/selectUser';
-import { RepositoryService } from '../services/RepositoryService';
-import { Link } from 'react-router-dom';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
-const mapStateToProps = state => {
-    return { 
-        specifications: state.specifications,
-        spec: state.spec,
-        instances: state.instances,
-        name: state.name,
-        users: state.users,
-        user: state.user,
-     };
-};
+const mapStateToProps = state => ( {
+    specifications: state.specifications,
+    spec: state.spec,
+    instances: state.instances,
+    name: state.name,
+    users: state.users,
+    user: state.user,
+} );
 
-const mapDispatchToProps = dispatch => {
-    return {
-        setSpecifications: (specifications) => dispatch(setSpecifications(specifications)),
-        selectSpecification: spec => dispatch(selectSpecification(spec)),
-        setInstances: (instances) => dispatch(setInstances(instances)),
-        selectInstance: name => dispatch(selectInstance(name)),
-        setUsers: (users) => dispatch(setUsers(users)),
-        selectUser: (user) => dispatch(selectUser(user)),
-    };
-  };
+const mapDispatchToProps = dispatch => ( {
+    setSpecifications: specifications => dispatch(setSpecifications(specifications)),
+    selectSpecification: spec => dispatch(selectSpecification(spec)),
+    setInstances: instances => dispatch(setInstances(instances)),
+    selectInstance: name => dispatch(selectInstance(name)),
+    setUsers: users => dispatch(setUsers(users)),
+    selectUser: user => dispatch(selectUser(user)),
+} );
 
 class ConnectedHeader extends React.Component {
     constructor(props) {
@@ -74,31 +73,22 @@ class ConnectedHeader extends React.Component {
     }
 
     render() {
-        const specs = this.props.specifications.map(spec => 
-            <MenuItem eventKey={spec.specId}> 
-                <Link onClick={() => {this.selectSpecification(spec)}}
-                    to={`/specifications/spec/${spec.specId}`}>
-                    {spec.name}
-                </Link>
-            </MenuItem>
+        const specs = this.props.specifications.map( spec =>
+            <LinkContainer key={spec.specId} to={`/specifications/spec/${spec.specId}`}>
+                <MenuItem eventKey={spec.specId} onClick={() => {this.selectSpecification(spec)}}>{spec.name}</MenuItem>
+            </LinkContainer>
         );
 
-        const instances = this.props.instances.map(i =>
-            <MenuItem eventKey={i.name}>
-                <Link onClick={() => {this.props.selectInstance(i.name)}}
-                    to={`/specifications/instances/instance/${i.name}`}>
-                    {i.name}
-                </Link>
-            </MenuItem>
+        const instances = this.props.instances.map( ( i, index ) =>
+            <LinkContainer key={`${i.name}--${index}`} to={`/specifications/instances/instance/${i.name}`}>
+                <MenuItem eventKey={i.name} onClick={() => {this.props.selectInstance(i.name)}}>{i.name}</MenuItem>
+            </LinkContainer>
         );
 
-        const users = this.props.users.map(user => 
-            <MenuItem eventKey={user.username}> 
-                <Link onClick={() => {this.selectUser(user)}}
-                    to={`/`}>
-                    {user.username}
-                </Link>
-            </MenuItem>
+        const users = this.props.users.map( user =>
+            <LinkContainer key={user.username} to='/'>
+                <MenuItem eventKey={user.username} onClick={() => {this.selectUser(user)}}>{user.username}</MenuItem>
+            </LinkContainer>
         );
 
         return (
@@ -112,7 +102,9 @@ class ConnectedHeader extends React.Component {
                 <Navbar.Collapse>
                     <Nav>
                         <NavDropdown eventKey={1} title="Specification" id="basic-nav-dropdown">
-                            <MenuItem eventKey={1.1}><Link onClick={() => {this.props.selectSpecification({})}} to='/specifications'>Manage Specifications</Link></MenuItem>
+                            <LinkContainer to='/specifications'>
+                                <MenuItem eventKey={1.1} onClick={() => {this.props.selectSpecification({})}}>Manage Specifications</MenuItem>
+                            </LinkContainer>
                             <MenuItem divider />
                             {specs}
                         </NavDropdown>
@@ -122,13 +114,19 @@ class ConnectedHeader extends React.Component {
                             {this.props.spec.name}
                         </NavItem>
                         {this.props.user.username === "Admin" && <NavDropdown eventKey={3} title="Designer" id="basic-nav-dropdown">
-                            <MenuItem eventKey={3.1}><Link to={`/specifications/designer/datamodel`}>Data Model</Link></MenuItem>
+                            <LinkContainer to='/specifications/designer/datamodel'>
+                                <MenuItem eventKey={3.1}>Data Model</MenuItem>
+                            </LinkContainer>
                             <MenuItem eventKey={3.2}>State Model</MenuItem>
-                            <MenuItem eventKey={3.3}><Link to={`/specifications/designer/goalmodel`}>Goal Model</Link></MenuItem>
+                            <LinkContainer to='/specifications/designer/goalmodel'>
+                                <MenuItem eventKey={3.3}>Goal Model</MenuItem>
+                            </LinkContainer>
                             <MenuItem eventKey={3.4}>Activity Model</MenuItem>
                         </NavDropdown>}
                         <NavDropdown eventKey={4} title="Instance" id="basic-nav-dropdown">
-                            <MenuItem eventKey={4.1}><Link onClick={() => {this.props.selectInstance('')}} to='/specifications/instances'>Manage Instances</Link></MenuItem>
+                            <LinkContainer to='/specifications/instances'>
+                                <MenuItem eventKey={4.1} onClick={() => {this.props.selectInstance('')}}>Manage Instances</MenuItem>
+                            </LinkContainer>
                             <MenuItem divider />
                             {instances}
                         </NavDropdown>
@@ -138,22 +136,28 @@ class ConnectedHeader extends React.Component {
                             {this.props.name}
                         </NavItem>
                         <NavDropdown eventKey={6} title="Executor" id="basic-nav-dropdown">
-                            <MenuItem eventKey={6.1}><Link to={`/specifications/executor/data`}>Data View</Link></MenuItem>
-                            <MenuItem eventKey={6.2}><Link to={`/specifications/executor/goals`}>Goal View</Link></MenuItem>
-                            <MenuItem eventKey={6.3}><Link to={`/specifications/executor/activities`}>Activity View</Link></MenuItem>
+                            <LinkContainer to='/specifications/executor/data'>
+                                <MenuItem eventKey={6.1}>Data View</MenuItem>
+                            </LinkContainer>
+                            <LinkContainer to='/specifications/executor/goals'>
+                                <MenuItem eventKey={6.2}>Goal View</MenuItem>
+                            </LinkContainer>
+                            <LinkContainer to='/specifications/executor/activities'>
+                                <MenuItem eventKey={6.3}>Activity View</MenuItem>
+                            </LinkContainer>
                         </NavDropdown>
                     </Nav>}
-                    {this.props.users && <Nav pullRight>
-                        {this.props.user && <NavItem eventKey={8}>
-                            <Link to={`/dashboard`}>{this.props.user.username}</Link>
-                        </NavItem>}
+                    {(this.props.users || []).length && <Nav pullRight>
+                        {(this.props.user || {}).username && <LinkContainer to='/dashboard'>
+                            <NavItem eventKey={8}>{this.props.user.username}</NavItem>
+                        </LinkContainer>}
                         <NavDropdown eventKey={7} title="Users" id="basic-nav-dropdown">
-                            {users || <MenuItem eventKey={7.1}>No users</MenuItem>}
+                            {users}
                         </NavDropdown>
                     </Nav>}
                 </Navbar.Collapse>
             </Navbar>
-        )
+        );
     }
 }
 
