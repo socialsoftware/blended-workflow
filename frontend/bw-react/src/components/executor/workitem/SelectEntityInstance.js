2cfa76001f017431b620ea6844531447d19ddff2
BernardoFuret <bernardo.furet@tecnico.ulisboa.pt>
2019-02-07 19:51:16 +0000

    General cleanup and added default dummy value for select-option.

--- frontend/bw-react/src/components/executor/workitem/SelectEntityInstance.js
+++ frontend/bw-react/src/components/executor/workitem/SelectEntityInstance.js
@@ -11,41 +11,16 @@ export class SelectEntityInstance extends React.Component {
             selected: props.currInstance || this.props.entityInstances[ 0 ],
         };
 
-        this.handleStartSelection = this.handleStartSelection.bind(this);
-        this.handleSelect = this.handleSelect.bind(this);
-
         this.handleSelectOption = this.handleSelectOption.bind(this);
     }
 
-    handleStartSelection() {
-        this.setState({
-            select: true,
-            selected: {}
-        });
-    }
-
-    handleSelect(event) {
-        const element = this.props.entityInstances.find(ei => ei.id === event.target.value);
-        this.setState({
-            select: false,
-            selected: element
-        }, function() {
-            this.props.onSelection(this.state.selected);
-        });
-    }
-
-    /*render() {
-        const notSelected = this.props.entityInstances.filter(ei => ei.id !== this.state.selected.id);
-        return (
-            <span>
-                <span>{!this.state.select && notSelected.length > 0 && <button onClick={this.handleStartSelection}>Start Selection</button>}</span>
-                <span>{this.state.select && notSelected.map(ei => <span key={ei.id} ><EntityInstanceLink key={ei.id} entityInstance={ei} /> <Button bsStyle="primary" value={ei.id} onClick={this.handleSelect} >Select</Button> </span>)}</span>
-            </span>
-        )
-    }*/
-
     handleSelectOption( e ) {
         const index = e.nativeEvent.target.selectedIndex;
+
+        if ( index === 0 ) {
+        	// Default
+        	return;
+        }
 
         this.setState( {
             select: true,
@@ -58,12 +33,20 @@ export class SelectEntityInstance extends React.Component {
     }
 
     render() {
+    	const options = [];
+
+    	if ( this.props.entityInstances.length ) {
+    		options.push( <option key={'--'} value={'--'}>{'--'}</option> );
+    	}
+
+    	[].push.apply( options, this.props.entityInstances
+			.map( (ei, i) => <option key={ei.id} value={ei.id}>{ei.id}</option> )
+        );
+
         return (
             <span>
-                <select value={( this.state.selected || {}).id} onChange={this.handleSelectOption}>{
-                    this.props.entityInstances
-                        //.filter( ei => ei.id !== this.state.selected.id )
-                        .map( (ei, i) => <option key={ei.id} value={ei.id}>{ei.id}</option> )
+                <select value={( this.state.selected || {} ).id} onChange={this.handleSelectOption}>{
+                    options
                 }</select>
                 {
                     this.state.selected && <EntityInstanceLink key={this.state.selected.id} entityInstance={this.state.selected} />
